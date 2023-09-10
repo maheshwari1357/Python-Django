df4774186a6181beee4713f15d7dbcbe3a19834e
karan.maheshwari <karan.maheshwari@globallogic.com>
2023-09-10 14:38:00 +0530

    HTML view added

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns=[
+    path("",views.index),
     path("<int:month>",views.monthly_challenge_num),
-    path("<str:month>", views.monthly_challenge)
+    path("<str:month>", views.monthly_challenge, name="month-challenge")
     ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_dict={
     "jan":"January",
@@ -7,6 +8,15 @@ monthly_dict={
     "apr":"April",
     "may":"May"
 }
+
+def index(request):
+    list_items=""
+    month=list(monthly_dict.keys())
+    for i in month:
+        month_path=reverse("month-challenge",args=[i])
+        list_items+=f"<li><a href=\"{month_path}\">{i.capitalize()}</a></li>"
+    response_data=f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
 
 def monthly_challenge_num(request, month):
     redirect_month=list(monthly_dict.keys())[month-1]
