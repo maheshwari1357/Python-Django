006e6b70270e2fc1c559885082e6c705ce1cb7c0
karan.maheshwari <karan.maheshwari@globallogic.com>
2023-09-10 12:23:35 +0530

    Dynamic URL's created

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
-# Create your views here.
+def january(request):
+    return HttpResponse("Welcome to the Homepage of MyPage!!")
+
+def feb(request):
+    return HttpResponse("Welcome to the Second Homepage of MyPage!!")
+
+def monthly_challenge(request,month):
+    response=None
+    if month== "January":
+        response="January"
+    elif month=="Feb":
+        response="February"
+    else:
+        return HttpResponseNotFound("Not supported")
+    return HttpResponse(response)
